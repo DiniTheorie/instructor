150a898222c769d67d9caa9e8e57043d28f68493
Florian Moser <git@famoser.ch>
2023-03-27 12:16:45 +0200

    Improve deployment

--- client/build/fontawesome.ts
+++ client/build/fontawesome.ts
@@ -1,4 +1,6 @@
-import { library as FontawesomeLibrary } from '@fortawesome/fontawesome-svg-core'
+import { config, library as FontawesomeLibrary } from '@fortawesome/fontawesome-svg-core'
 import { faPencil, faPlus, faTrash } from '@fortawesome/pro-light-svg-icons'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 
+config.autoAddCss = false
 FontawesomeLibrary.add(faPlus, faPencil, faTrash)
