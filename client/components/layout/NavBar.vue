d3eb148092a6ee1d28a94f85652ad5ba345d4832
Florian Moser <git@famoser.ch>
2023-03-29 12:59:53 +0200

    Remove unused settings

--- client/components/layout/NavBar.vue
+++ client/components/layout/NavBar.vue
@@ -1,17 +1,7 @@
 <script setup lang="ts">
-import { ref, watchEffect } from 'vue'
 import { useI18n } from 'vue-i18n'
-import { useSettingsStore } from '@/stores/localeStore'
 import { api } from '@/services/api'
 import SubmitButton from '@/components/shared/LoadingButton.vue'
-
-const language = ref('en')
-const supportedLanguages: string[] = ['en', 'fr', 'it']
-
-const settings = useSettingsStore()
-watchEffect(() => {
-  settings.setTranslationLanguage(language.value)
-})
 
 const publishChanges = async () => {
   await api.store()
@@ -49,14 +39,6 @@ const { t } = useI18n()
           <SubmitButton :submit="publishChanges">{{ t('layout.nav_bar.publish_changes') }}</SubmitButton>
           <SubmitButton class="btn-danger" :submit="discardChanges">{{ t('layout.nav_bar.discard_changes') }}</SubmitButton>
         </div>
-        <form class="d-flex ms-4">
-          <label class="col-form-label me-2" for="language-select">{{ t('layout.nav_bar.translation_language') }}</label>
-          <select class="form-select" v-model="language" id="language-select" aria-label="Default select example">
-            <option v-for="language in supportedLanguages" :key="language" :value="language">
-              {{ t('domain.supported_language.' + language) }}
-            </option>
-          </select>
-        </form>
       </div>
     </div>
   </nav>
